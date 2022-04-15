4ac6bcff4112ac10a61e3d4fe7757071ac8f7ef5
unknown <hugosilva905@gmail.com>
2022-04-15 14:26:26 -0300

    DepartmentList view desing

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,37 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable {
+	
+	
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
